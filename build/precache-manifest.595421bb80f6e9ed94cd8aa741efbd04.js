8fd9ec3580bfab91583c5295e3f86f668309236e
ktleary <kevin@ktleary.com>
2020-05-11 01:28:53 -0400

    built site

---
+++ build/precache-manifest.595421bb80f6e9ed94cd8aa741efbd04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9f3e24a18c717c2b8a4ca2b14ea93e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "441458909c165ebaf751",
+    "url": "/static/css/main.ea0a9b3d.chunk.css"
+  },
+  {
+    "revision": "29b66606f993cec22047",
+    "url": "/static/js/2.e9053e8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e9053e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441458909c165ebaf751",
+    "url": "/static/js/main.27f5faea.chunk.js"
+  },
+  {
+    "revision": "6455db24c1cc387be16a",
+    "url": "/static/js/runtime-main.9ea24f03.js"
+  }
+]);
