80a0e4bf6ea157aa1018c89fece1ecf8845dfa13
ktleary <kevin@ktleary.com>
2020-05-11 02:04:02 -0400

    add font

---
+++ build/precache-manifest.44623b56f370d88030aa1497bd0671e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6423c1866ebe3218054602fe6b38478d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f0b51b1de2f19968a01",
+    "url": "/static/css/main.a4bd7c84.chunk.css"
+  },
+  {
+    "revision": "29b66606f993cec22047",
+    "url": "/static/js/2.e9053e8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e9053e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0b51b1de2f19968a01",
+    "url": "/static/js/main.27f5faea.chunk.js"
+  },
+  {
+    "revision": "6455db24c1cc387be16a",
+    "url": "/static/js/runtime-main.9ea24f03.js"
+  }
+]);
