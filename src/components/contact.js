0c8b7b3c641a7429ba0a8a10d48d3d582d15a415
ktleary <kevin@ktleary.com>
2021-02-26 23:49:33 +0800

    simpllify mouseenter/leave

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { ContactHelmet } from "./helmet";
-import { always, compose, curry, equals, find, prop } from "ramda";
-import { nanoid } from "nanoid";
+import { compose, curry, equals, find, prop } from "ramda";
 import { copyToClipboard } from "../util";
 import { Cell } from "./cell-row";
 import { AngelListIcon, EmailIcon, CopyIcon } from "./icons";
@@ -51,28 +50,48 @@ const contactData = [
   { name: "Email", url: "mailto:kevin@ktleary.com", icon: <EmailIcon /> },
 ];
 
+const SelectedButton = ({ name }) => (
+  <Cell data-testid={`link-cell-${name}`}>
+    {equals(name, "AngelList") ? (
+      <AngelListButton />
+    ) : (
+      <EmailContainer>
+        <EmailButton />
+      </EmailContainer>
+    )}
+  </Cell>
+);
+
+const CopyEmail = ({ handleCopyEmail, copyBackground }) => (
+  <Cell
+    onClick={handleCopyEmail}
+    style={{ background: copyBackground, borderRadius: 16 }}
+  >
+    <CopyIcon title="Copy" data-testid="copy-icon" />
+  </Cell>
+);
+
 // -- Util ------
 
 const getRepoEmail = (contactData) =>
   splitMailto(prop("url", find(nameIsEmail, contactData)));
 const getAndCopyEmail = compose(copyToClipboard, getRepoEmail);
 const contactNameEqUrl = (name, contact) => equals(prop("name", contact), name);
-const curriedContactNameEqUrl = curry(contactNameEqUrl);
-const contactNameEqUrlWName = (name) => curriedContactNameEqUrl(name);
+const dontactNameEqUrlWName = curry(contactNameEqUrl);
+// const contactNameEqUrlWName = (name) => curriedContactNameEqUrl(name);
 const getUrl = (e) =>
-  prop("url", find(contactNameEqUrlWName(getNameAtt(e)), contactData));
+  prop("url", find(dontactNameEqUrlWName(getNameAtt(e)), contactData));
 const getOpenUrl = compose(window.open, getUrl);
 
-function ContactLink({ name }) {
+const Contact = () => {
   const [showCopy, setShowCopy] = useState(false);
   const [copyBackground, setCopyBackground] = useState(COPYSTATES.NORMAL);
-  const handleShowCopy = () => setShowCopy(true);
-  const handleHideCopy = () =>
-    setCopyBackground("transparent", setShowCopy(false));
-  const resetCopyBackground = () =>
-    setCopyBackground(prop("NORMAL", COPYSTATES));
+
   const copyMessage = (result) =>
-    setCopyBackground(result, setTimeout(resetCopyBackground, 333));
+    setCopyBackground(
+      result,
+      setTimeout(() => setCopyBackground(prop("NORMAL", COPYSTATES)), 333)
+    );
 
   const handleCopyEmail = async (e) => {
     e.stopPropagation();
@@ -81,45 +100,44 @@ function ContactLink({ name }) {
       : copyMessage(prop("FAIL", COPYSTATES));
   };
 
-  return (
-    <LinkRow
-      name={name}
-      onMouseOver={equals(name, "Email") ? handleShowCopy : null}
-      onMouseLeave={equals(name, "Email") ? handleHideCopy : null}
-      onClick={getOpenUrl}
-      data-testid={`link-row-${name}`}
-    >
-      <Cell data-testid={`link-cell-${name}`}>
-        {equals(name, "AngelList") ? (
-          <AngelListButton />
-        ) : (
-          <EmailContainer>
-            <EmailButton />
-          </EmailContainer>
-        )}
-      </Cell>
-      <Cell>{name}</Cell>
-      {name === "Email" && showCopy && (
-        <Cell
-          onClick={handleCopyEmail}
-          style={{ background: copyBackground, borderRadius: 16 }}
-        >
-          <CopyIcon title="Copy" data-testid="copy-icon" />
-        </Cell>
-      )}
-    </LinkRow>
-  );
-}
+  const hideShowCopy = (show) =>
+    show
+      ? setShowCopy(true)
+      : setCopyBackground("transparent", setShowCopy(false));
 
-const Contact = always(
-  <ContactContainer>
-    <ContactHelmet />
-    <Links>
-      {contactData.map((contact) => (
-        <ContactLink name={contact.name} url={contact.url} key={nanoid()} />
-      ))}
-    </Links>
-  </ContactContainer>
-);
+  const handleMouseOver = (e) => {
+    console.log(equals(getNameAtt(e), "Email"));
+    return equals(getNameAtt(e), "Email") ? hideShowCopy(true) : false;
+  };
+
+  const handleMouseLeave = (e) =>
+    equals(getNameAtt(e), "Email") ? hideShowCopy(false) : false;
+
+  return (
+    <ContactContainer>
+      <ContactHelmet />
+      <Links>
+        {contactData.map((contact) => (
+          <LinkRow
+            name={prop("name", contact)}
+            onMouseEnter={handleMouseOver}
+            onMouseLeave={handleMouseLeave}
+            onClick={getOpenUrl}
+            data-testid={`link-row-${prop("name", contact)}`}
+          >
+            <SelectedButton name={prop("name", contact)} />
+            <Cell>{prop("name", contact)}</Cell>
+            {prop("name", contact) === "Email" && showCopy && (
+              <CopyEmail
+                handleCopyEmail={handleCopyEmail}
+                copyBackground={copyBackground}
+              />
+            )}
+          </LinkRow>
+        ))}
+      </Links>
+    </ContactContainer>
+  );
+};
 
 export default Contact;
